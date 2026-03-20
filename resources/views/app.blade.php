8e7b72fd24fcc0911c110913dcf126430ef358ec
Resyorc <blaskishare@gmail.com>
2026-03-20 17:03:30 +0700

    fix: update favicon links to use secure_asset for improved security

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -43,9 +43,9 @@
 
     <title inertia>{{ config('app.name', 'Nuvelo') }}</title>
 
-    <link rel="icon" href="/favicon.ico" sizes="any">
-    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <link rel="icon" href="{{ secure_asset('favicon.ico') }}" sizes="any">
+    <link rel="icon" href="{{ secure_asset('favicon.svg') }}" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="{{ secure_asset('apple-touch-icon.png') }}">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
